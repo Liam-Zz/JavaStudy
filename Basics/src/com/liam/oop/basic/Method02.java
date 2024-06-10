0d44eefe6bb4e8b0f4eaeb31ddcc87b0a2858564
liam <seniorman1206@gmail.com>
2024-06-11 00:02:55 +0800

    GitHub Study Test

---
+++ Basics/src/com/liam/oop/basic/Method02.java
@@ -0,0 +1,62 @@
+package com.liam.oop.basic;
+
+public class Method02 {
+    public static void main(String[] args) {
+        /*
+            方法的定义:
+                public 返回数据类型 方法名(形参列表...) {
+                    方法体;
+                    return 返回值;
+                }
+
+                形参列表: 表示成员方法输入
+                返回数据类型(返回类型): 表示成员方法输出,void 表示没有返回值
+                方法主体: 表示为了实现某一功能代码块
+                return 语句不是必须的
+         */
+
+        /*
+            方法细节
+                访问修饰符: 作用是控制方法使用的范围
+
+            返回数据类型
+                1.一个方法最多有一个返回值,多个返回值可使用数组
+                2.返回值类型可以为任意类型(基本数据类型,引用类型)
+                3.如果方法要求有返回值,则方法体中最后的执行语句必须为 return 值,且返回值类型与 return 值类型相同(遵循类型转换)
+                4.方法名命令建议遵循驼峰命令法,见名知意
+
+            形参列表
+                1.一个方法可以有 0 个参数,也可以有多个参数
+                2.参数类型可以为任意类型
+                3.调用带参数的方法时,一定对应着参数列表传入相同类型或兼容类型的参数
+                4.方法定义时的参数成为形式参数,方法调用时的参数称为实际参数
+                5.实参和形参的类型要兼容,个数和顺序必须一致
+
+            方法体
+                1.方法不能被嵌套定义
+
+            方法调用
+                1.同一个类中的方法可以直接调用
+                2.跨类调用方法,需要通过对象名调用
+                3.跨类的方法调用和方法的访问修饰符有关
+         */
+        AA aa = new AA();
+        int[] sumAndSub = aa.getSumAndSub(1, 4);
+        for (int i = 0; i < sumAndSub.length; i++) {
+            System.out.print(sumAndSub[i] + "\t");
+        }
+        System.out.println("\n=== 展示=== ");
+        System.out.println("和 : " + sumAndSub[0]);
+        System.out.println("差 : " + sumAndSub[1]);
+    }
+}
+
+class AA{
+    public int[] getSumAndSub(int n1,int n2){
+        int[] resArr = new int[2];
+
+        resArr[0] = n1 + n2;
+        resArr[1] = n1 - n2;
+        return resArr;
+    }
+}
--- Basics/src/com/liam/oop/basic/PersonDemo.java
+++ Basics/src/com/liam/oop/basic/PersonDemo.java
@@ -23,12 +23,88 @@ public class PersonDemo {
         persion02.age = 200;
         persion02 = null;
         System.out.println(persion01.age);
-        System.out.println(persion02.age);
+        //System.out.println(persion02.age);
+
+        System.out.println("==== 练习三 ====");
+        //调用 speak 方法
+        persion01.speak();
+
+        System.out.println("==== 练习四 ====");
+        persion01.cal01();
+
+        System.out.println("==== 练习五 ====");
+        persion01.cal02(99);
+
+        System.out.println("==== 练习六 ====");
+        int getsun = persion01.cal03(40, 20);
+        System.out.println("累积合为: " + getsun);
+
 
     }
 }
 
 class Persion{
+    //属性
     String name;
     int age;
+
+    //方法
+    /*
+        1.添加成员方法,实现输出一句话
+
+            public: 表示方法是公开的
+            void: 表示方法没有返回值
+            speak(): speak 是方法名, () 存放形参列表
+            {} : 方法体(需要执行的代码)
+
+            方法通过对象调用,如果不调用则不会被执行
+
+     */
+    /*
+        常见方法实现输出与句话
+     */
+    public void speak(){
+        System.out.println("我是一个好人");
+    }
+
+    /*
+        创建方法实现计算 1~10000 的累积
+     */
+    public void cal01(){
+        int res = 0;
+        for (int i = 0; i <= 1000; i++) {
+            res+= i;
+        }
+        System.out.println("计算结果: " + res);
+    }
+
+    /*
+        创建方法实现计算 1~n 的累积
+     */
+    public void cal02( int n){
+        int res = 0;
+        for (int i = 0; i <= n; i++) {
+            res+= i;
+        }
+        System.out.println("计算结果: " + res);
+    }
+
+    /*
+        创建方法实现计算两个数的和
+        int : 表示方法执行后,返回一个 int 值
+        return: 表示把 res 的值返回;
+
+        方法调用原理图
+
+            1.当程序执行到方法是,就会开辟一个独立的空间(栈空间)
+            2.当方法执行完毕,或者执行到 return 语句时,就会返回(返回调用方法的地方)
+            3.方法调用值返回后,继续指定方法后面的代码
+            4.当 main 方法(栈)执行完毕后,整个程序退出
+     */
+    public int cal03(int num1,int num2) {
+        //此处代码可简化
+        //int res = num1 + num2;
+        //return res;
+        return num1+num2;
+    }
 }
