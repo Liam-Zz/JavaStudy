250e9954fc565555675c70788ca7035114ee9c6c
liam <seniorman1206@gmail.com>
2024-06-12 08:34:37 +0800

    GitHub Study Test

---
+++ Basics/src/com/liam/oop/basic/recursion/Recursion01.java
@@ -0,0 +1,20 @@
+package com.liam.oop.basic.recursion;
+
+public class Recursion01 {
+    public static void main(String[] args) {
+        /*
+            打印问题
+         */
+        CC cc = new CC();
+        cc.test(4);
+    }
+}
+
+class CC{
+    public void test(int n){
+        if (n >2) {
+            test(n-1);
+        }
+            System.out.println("n= " + n);
+    }
+}
