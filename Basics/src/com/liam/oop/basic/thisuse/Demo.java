1889cfb33f1170e529ff3b2176c32b66bbbb263d
Liam <seniorman1206@gmail.com>
2024-06-19 17:23:22 +0800

    0619

---
+++ Basics/src/com/liam/oop/basic/thisuse/Demo.java
@@ -0,0 +1,11 @@
+package com.liam.oop.basic.thisuse;
+
+public class Demo {
+    /*
+        java虚拟机会给每个对象分配一个this，代表当前对象；
+
+        我的眼;
+            小明说：那么眼睛就是小明的
+            小红说：那么眼睛就是小红的
+     */
+}
