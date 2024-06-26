241006dbb0d06788df286590fd2983a91733cfc2
Liam <seniorman1206@gmail.com>
2024-06-26 17:29:37 +0800

    0624

---
+++ JavaWeb/src/com/liam/servlet/ServletConfigServlet01.java
@@ -0,0 +1,52 @@
+package com.liam.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(
+        urlPatterns = "/config1",
+        initParams = {@WebInitParam(name="key1",value = "value1"),@WebInitParam(name="key2",value = "value2")}
+)
+public class ServletConfigServlet01 extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("config 获取参数-----------------------------------------------");
+        //获取servletconfig信息
+        ServletConfig servletConfig = this.getServletConfig();
+        //获取初始配置信息（根据参数名获取参数值）
+        String keya = servletConfig.getInitParameter("keya");
+        //System.out.println(keya);
+        //如果不知道参数值，获取所有的参数名
+        Enumeration<String> initParameterNames = servletConfig.getInitParameterNames();
+
+            while (initParameterNames.hasMoreElements()){
+                String kname = initParameterNames.nextElement();
+                System.out.println(kname+"="+getInitParameter(kname));
+        }
+        System.out.println("config 获取参数-----------------------------------------------");
+            //获取context对象
+        ServletContext servletContext = getServletContext();
+        ServletContext servletContext1 = servletConfig.getServletContext();
+        ServletContext servletContext2 = req.getServletContext();
+        System.out.println(servletContext == servletContext1);
+        System.out.println(servletContext1 == servletContext2);
+
+        String encoding = servletContext.getInitParameter("encoding");
+        System.out.println("encoding: "+encoding);
+
+        Enumeration<String> initParameterNames1 = servletContext.getInitParameterNames();
+        while (initParameterNames1.hasMoreElements()){
+            String pname = initParameterNames1.nextElement();
+            System.out.println(pname+"="+servletContext+getInitParameter(pname));
+        }
+
+    }
+}
---
+++ JavaWeb/src/com/liam/servlet/ServletUpload.java
@@ -0,0 +1,29 @@
+package com.liam.servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileOutputStream;
+import java.io.IOException;
+@WebServlet("/upload")
+public class ServletUpload extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //向upload目录写入一个文件
+        //在文件上传处会使用
+        //String path = "D:\\02_Liam\\Java\\JavaStudy\\out\\artifacts\\JavaWeb_war_exploded\\a.txt";
+        ServletContext servletContext = getServletContext();
+        //获得一个指向项目部署位置下的某个文件/目录的磁盘真是路径API
+        String realPath = servletContext.getRealPath("upload");
+        System.out.println(realPath);
+
+        //获得项目部署的上下文路径，项目的访问路径
+        //后续我们会学习在项目中使用相对路径和绝对路径找目标资源
+        String contextPath = servletContext.getContextPath();
+        System.out.println(contextPath);
+
+    }
+}
